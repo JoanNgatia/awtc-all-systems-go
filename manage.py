62550663ba176a31d024aea4346579282285476a
andela-jngatia <joan.ngatia@andela.com>
2016-11-12 00:04:34 +0300

    (Feature)Implement User registration

--- manage.py
+++ manage.py
@@ -1,7 +1,8 @@
 """Flask application instatiation, server entry point."""
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy.exc import SQLAlchemyError
+import models
 
 app = Flask(__name__)
 
@@ -13,6 +14,26 @@ db = SQLAlchemy(app)
 @app.route('/')
 def main():
     return render_template("index.html")
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.form['username']
+    password = request.form['password']
+    email = request.form['email']
+    exists = models.User.query.filter_by(username=username).first()
+    if username and password:
+        if exists:
+            return redirect(url_for('main'))
+        new_user = models.User(username=username, email=email)
+        new_user.hash_password(password)
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect('/later')
+        except SQLAlchemyError:
+            db.session.rollback()
+        return redirect(url_for('main'))
 
 
 @app.route('/later')
